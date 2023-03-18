9f63d841c0e164b6ae879b1c53bddd1a8f5847e3
Taslim <harountaslim@gmail.com>
2023-03-17 20:20:26 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 int main(void)
 {
-	int digit1, digit2, digit3;
+        int x, y, z;
 
-	for (digit1 = 0; digit1 < 8; digit1++)
-	{
-		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
-		{
-			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
-			{
-				putchar((digit1 % 10) + '0');
-				putchar((digit2 % 10) + '0');
-				putchar((digit3 % 10) + '0');
+        for (x = 0; y < 8; x++)
+        {
+                for (y = x + 1; y < 9; y++)
+                {
+                        for (z = y + 1; z < 10; z++)
+                        {
+                                putchar((x % 10) + '0');
+                                putchar((y % 10) + '0');
+                                putchar((z % 10) + '0');
 
-				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					continue;
-				
-				putchar(',');
-				putchar(' ');
+                                if (x == 7 && y == 8 && z == 9)
+                                        continue;
 
-			}
-		}
-	}
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+        }
 
-	putchar('\n');
-
-	return (0);
+        putchar('\n');
+        return (0);
 }
